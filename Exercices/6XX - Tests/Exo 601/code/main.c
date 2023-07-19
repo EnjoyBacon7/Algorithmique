a0bd2e6fe2bb0dec1a81064ecd8d21795f0dcc3a
EnjoyBacon7 <59032058+EnjoyBacon7@users.noreply.github.com>
2023-07-19 15:30:50 +0200

    Added Ex. 601

---
+++ Exercices/6XX - Tests/Exo 601/code/main.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+int main()
+{
+
+    int saisie;
+
+    printf("Entrez un entier: ");
+    scanf("%d", &saisie);
+
+    int reponse = 0;
+
+    if (saisie % 5 == 0)
+        reponse += 1;
+    if (saisie >= 3)
+        reponse += 2;
+
+    if (reponse == 1)
+        printf("%d est un multiple de 5 seulement.\n", saisie);
+    else if (reponse == 2)
+        printf("%d est superieur ou egal a 3 seulement.\n");
+    else if (reponse == 3)
+        printf("%d est un multiple de 5 et superieur ou egal a 3.\n");
+    else
+        printf("%d n'est ni un multiple de 5 ni superieur ou egal a 3.\n");
+}
