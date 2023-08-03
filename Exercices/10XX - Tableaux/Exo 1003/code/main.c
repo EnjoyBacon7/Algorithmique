a66e870d4e7a9e72820a026eb816729a2e32ec3d
EnjoyBacon7 <59032058+EnjoyBacon7@users.noreply.github.com>
2023-08-03 18:37:25 +0200

    Added 1003

--- Exercices/10XX - Tableaux/Exo 1003/code/main.c
+++ Exercices/10XX - Tableaux/Exo 1003/code/main.c
@@ -1,8 +1,20 @@
 #include <stdio.h>
 
+void remplissage(float tab[10]);
+void affichage(float tab[10]);
+float initialisation(float tab[10]);
+void minMax(float tab[10], float* min, float* max);
+
 int main() {
 
+    float tab[10];
+    float min, max;
+    float moyenne = initialisation(tab);
+    minMax(tab, &min, &max);
 
+    printf("La moyenne est de %.2f\n", moyenne);
+    printf("La note minimale est de %.2f\n", min);
+    printf("La note maximale est de %.2f\n", max);
 
     return 0;
 }
@@ -13,7 +25,7 @@ void remplissage(float tab[10]) {
     {
         printf("Entrez la note numero %d: ", i+1);
         fflush(stdin);
-        scanf("%f", tab[i]);
+        scanf("%f", &tab[i]);
 
         if(tab[i] < 0 && tab[i] > 20) {
             printf("Valeur impossible.");
@@ -22,3 +34,45 @@ void remplissage(float tab[10]) {
     }
     
 }
+
+void affichage(float tab[10]) {
+
+    for (int i = 0; i < 10; i++)
+    {
+        printf("Note numero %d: %.2f\n", i+1, tab[i]);
+    }
+    
+}
+
+float initialisation(float tab[10]) {
+
+    remplissage(tab);
+    affichage(tab);
+    
+    float moyenne = 0;
+    for(int i = 0; i < 10; i++) {
+        moyenne += tab[i];
+    }
+    moyenne /= 10;
+
+    return moyenne;
+
+}
+
+void minMax(float tab[10], float* min, float* max) {
+
+    *min = tab[0];
+    *max = tab[0];
+
+    for(int i = 0; i < 10; i++) {
+        if(tab[i] < *min) {
+            *min = tab[i];
+        }
+        if(tab[i] > *max) {
+            *max = tab[i];
+        }
+    }
+
+}
+// Demander de retourner le min et le max dans la fonction minMax est impossible car on ne peut retourner qu'une seule valeur.
+// On ne peut pas non plus retourner de tableau. Il faut donc passer par des pointeurs.
