b14db122ac318925530b50b0c3d6c1ed95e2112d
EnjoyBacon7 <59032058+EnjoyBacon7@users.noreply.github.com>
2023-08-04 22:20:48 +0200

    Added 1005

--- Exercices/10XX - Tableaux/Exo 1005/code/main.c
+++ Exercices/10XX - Tableaux/Exo 1005/code/main.c
@@ -1,6 +1,63 @@
 #include <stdio.h>
 
+void saisie(int tab[10]);
+void affichage(int tab[10]);
+void triInsertion(int tab[10]);
+void recherche(int tab[10], int x);
+
 int main() {
+
+    int tab[10];
+    saisie(tab);
+    affichage(tab);
+    triInsertion(tab);
+    affichage(tab);
+
+    int x;
+    printf("Entrez l'element a rechercher: \n");
+    scanf("%d", &x);
+    recherche(tab, x);
+
+    return 0;
 
 }
 
+void saisie(int tab[10]) {
+    int i;
+    for (i = 0; i < 10; i++) {
+        printf("Entrez un nombre : ");
+        scanf("%d", &tab[i]);
+    }
+}
+
+void affichage(int tab[10]) {
+    int i;
+    for (i = 0; i < 10; i++) {
+        printf("%d ", tab[i]);
+    }
+    printf("\n");
+}
+
+void triInsertion(int tab[10]) {
+    int i, j, tmp;
+    for (i = 1; i < 10; i++) {
+        tmp = tab[i];
+        j = i - 1;
+        while (j >= 0 && tab[j] > tmp) {
+            tab[j + 1] = tab[j];
+            j--;
+        }
+        tab[j + 1] = tmp;
+    }
+}
+
+void recherche(int tab[10], int x) {
+    int i;
+    for (i = 0; i < 10; i++) {
+        if (tab[i] == x) {
+            printf("L'element %d est trouve a l'indice %d", x, i);
+            return;
+        }
+    }
+    printf("L'element %d n'est pas trouve", x);
+}
